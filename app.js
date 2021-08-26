393731e48e2a71b8b70e8079466d879484d306af
Abbass <abbassmortazavi@gmail.com>
2021-08-26 13:34:36 +0430

    create register form

--- app.js
+++ app.js
@@ -2,11 +2,23 @@ const express = require('express');
 
 const app = express();
 const expressLayouts = require('express-ejs-layouts');
+const mongoose = require('mongoose');
 
+//connect to mongodb
+mongoose.connect('mongodb://localhost:27017/nodePassportLogin', { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        console.log('mongoose connected');
+    }).catch(err => {
+        console.log(err);
+    });
+mongoose.Promise = global.Promise;
 
 //ejs
 app.use(expressLayouts);
 app.set('view engine', 'ejs');
+
+//bodyParser
+app.use(express.urlencoded({extended:false}));
 
 //Route
 app.use('/', require('./routes/index'));
--- routes/user.js
+++ routes/user.js
@@ -10,4 +10,42 @@ router.get('/register', function (req, res) {
     res.render('register');
 });
 
+//Handle requests
+router.post('/register', function (req, res) {
+    const { name, email, password, password2 } = req.body;
+    let errors = [];
+
+    //check require filled
+    if (!email || !name || !password || !password2)
+    {
+        errors.push({
+            msg: 'Please all the fill input...'
+        });
+    }
+
+    //check password
+    if (password !== password2)
+    {
+        errors.push({ msg: 'password not match...' });
+    }
+
+    //check pass length
+    if (password.length < 6) {
+        errors.push({ msg: 'Password must be 6 characters!'})
+    }
+
+    if (errors.length > 0) {
+        res.render('register', {
+            errors,
+            name,
+            email,
+            password,
+            password2
+        });
+    } else {
+        res.send(req.body);
+    }
+    
+});
+
 module.exports = router;
